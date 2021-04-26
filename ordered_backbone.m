6dbb477baaff0febc057720a3270338f37943539
ThisIsNima <42547511+ThisIsNima@users.noreply.github.com>
2021-04-26 19:42:20 -0400

    Update ordered_backbone.m

--- ordered_backbone.m
+++ ordered_backbone.m
@@ -1,18 +1,18 @@
-%%%%%%%%%%%% upload files
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% upload files
 
-dir='/' % add the data directory
+dir='/' % enter the data directory (look at example data in the data directory)
 cd dir
 files = dir('*.mat');
 for i=1:length(files)
 data1= load(files(i).name); % eval(['load ' files(i).name ]);
-healthy_41{i}=data1.TSData.Region_41.allVoxts;
+healthy_41{i}=data1.TSData.Region_41.allVoxts;    %for region 41 (change the region number based on your interest)
 end
   
 %%%%%%%%%%%% create the dynamic network connectivity with given temporal window size
 
 Dynamic_Adjacencies=ST_Preparation(w_t);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%START
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% START
 
 
 clear Backbones
@@ -46,30 +46,30 @@ end
 end
 end
 
-data=Adjacencies_Healthy_41_normalized{1, N};
+data=Dynamic_Adjacencies_normalized{1, N};
 for i=1:length(Dynamic_C_Adjacencies{1, 1}{1, 1})
-for t=1:20
+for t=1:tau
 data{t,1}(i,i)=0;
 end
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Mean
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Mean
 clear arr1
-clear mean_vals_41
-for i=1:64
-for j=1:64
-for t=1:20
+clear mean_values
+for i=1:num_voxels
+for j=1:num_voxels
+for t=1:tau
 arr1(t)=data{t, 1}(i,j);
 end
 
 
-mean_vals_41(i,j)=mean(arr1);
+mean_values(i,j)=mean(arr1);
 end
 end
-for i=1:64
-mean_vals_41(i,i)=0;
+for i=1:num_voxels
+mean_values(i,i)=0;
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%initial values:
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Calculate Initial values:
 
 clear arr1
 clear arr2
@@ -79,9 +79,9 @@ clear init_denominator
 clear sum_rows
 clear total_sum
 
-%%%%%%%nominator
-for i=1:64
-for t=1:20
+%%%%%%%%%%%%%% nominator
+for i=1:num_voxels
+for t=1:tau
 arr1(t)=sum(data{t, 1}(i,:))-1;
 end
 
@@ -89,15 +89,15 @@ end
 init_nominator(i)=mean(arr1);
 end
 
-%%%%%%%denominator
-for i=1:64
-for t=1:20
+%%%%%%%%%%%%%% denominator
+for i=1:num_voxels
+for t=1:tau
 arr2(t)=sum(data{t, 1}(i,:))-1;
 end
 init_nominator(i)=mean(arr2);
 end
-for i=1:64
-for t=1:20
+for i=1:num_voxels
+for t=1:tau
 arr3(t)=sum(data{t, 1}(i,:))-1;
 end
 sum_rows(i)=sum(arr3);
@@ -105,79 +105,49 @@ end
 total_sum=sum(sum_rows);
 init_denominator=sqrt(total_sum);
 init_vals=init_nominator/init_denominator;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%MLE mean
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% MLE mean optimization
 
-fun = @(x) optimize_backbone(x,mean_vals_41);
+fun = @(x) optimize_backbone(x,mean_values);
     [estimated_mean,~,exitf,~] = fsolve(fun,init_vals');
 
 %'
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%std vals from estimatd means
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% std vals from estimatd means
 
 clear ar11
-clear std_vals
-for i=1:64
-for j=1:64
+clear std_values
+for i=1:num_voxels
+for j=1:num_voxels
 
-for t=1:20
-arr11(t)=(data{t, 1}(i,j)-(estimated_mean(i)*estimated_mean(j)))^2 ;
+for t=1:tau
+arr3(t)=(data{t, 1}(i,j)-(estimated_mean(i)*estimated_mean(j)))^2 ;
 end
 
-std_vals(i,j)=sqrt(sum(arr11)/20);
+std_values(i,j)=sqrt(sum(arr3)/tau);
 end
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%MLE std
-fun = @(x) optimize_backbone(x,std_vals);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% MLE std optimization
+fun = @(x) optimize_backbone(x,std_values);
     [estimated_std,~,exitf,~] = fsolve(fun,init_vals');
 
 % '
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Statistical test
-
-% MLE_mean_41{N, 1}=estimated_mean;
-% MLE_std_41{N, 1}=estimated_std;
-
-% clear arr1
-% clear mean_vals_41
-% for i=1:64
-% for j=1:64
-% for t=1:20
-% arr1(t)=data{t, 1}(i,j);
-% end
-% sum_weights(i,j)=sum(arr1);
-% end
-% end
-% for i=1:64
-% sum_weights(i,i)=0;
-% end
-% sum_weight{N, 1}=sum_weights;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Statistical test
 
 
-% for k=1:14
-% for i=1:64
-% sum_weight_nodes(i,k)= sum(sum_weight{k, 1}(i,:));
-% end
-% end
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
-
-for T=1:20
+for T=1:tau
 
 mat_41=data{T,1};
 
-for i=1:64
+for i=1:num_voxels
 mat_41(i,i)=0;
 end
 
 
 
-k=0.8;
+k=0.8; %backbone threshold
 
-z_t_41=zeros(64,64);
-for i=1:64
-for j=1:64
+z_t_41=zeros(num_voxels,num_voxels);
+for i=1:num_voxels
+for j=1:num_voxels
 
 m_41=estimated_mean(i)*estimated_mean(j);
 s_41=estimated_std(i)*estimated_std(j);
@@ -199,7 +169,7 @@ matrices_41{T, 1}=z_t_41;
 
 end % for T=1:20 loop
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%extract the backbone network
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Extract the backbone network
 
 clear count_ones
 
@@ -207,38 +177,31 @@ disp('extracting backbone...')
 
 
 
-for i=1:64
-for j=1:64
-
-for T=1:20
+for i=1:num_voxels
+for j=1:num_voxels
+for T=1:tau
 arr4(T)= matrices_41{T, 1}(i,j);
 end
-
 count_ones(i,j)=sum(arr4(:) == 1);
 end
 end
-
-
-
-
-
 Backbones{N, 1}=count_ones;
 
 
 
-%clear healthy_41_Final_Backbone
+%clear Final_Backbone
 
 disp('final backbone calculation...')
 
 
 
-for i=1:64
-for j=1:64
+for i=1:num_voxels
+for j=1:num_voxels
 
-if Backbones{N,1}(i,j)>=10
-healthy_41_Final_Backbone_20_95{N,1}(i,j)=1;
+if Backbones{N,1}(i,j)>=(tau/2)
+Final_Backbone{N,1}(i,j)=1;
 else
-healthy_41_Final_Backbone_20_95{N,1}(i,j)=0;
+Final_Backbone{N,1}(i,j)=0;
 end
 
 end
