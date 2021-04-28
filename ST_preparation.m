398073b5b373f9a25a2d2c7e845aa45f6d3668e1
ThisIsNima <42547511+ThisIsNima@users.noreply.github.com>
2021-04-27 20:49:17 -0400

    Update ST_preparation.m

--- ST_preparation.m
+++ ST_preparation.m
@@ -1,42 +1,45 @@
+function [Dynamic_Adjacency] = ST_preparation(data_set, w_t)
 
+% Inputs:
+%   w_t - the window size of temporal segmentation.
+%
+%   data_set - the window size of temporal segmentation.
+% Outputs:
+%  Dynamic_Adjacency - The set of adjacency matrices for dynamic connectivity of the network
+%
+t=length(data_Set{1,1}); %length of time series
+tau= t/w_t; % number of temporal segmentations
+num_voxel=size(data_set{1,1},1 ); %number of voxels in region of interest
 
-for f=1:15
-
-x_raw_41=healthy_41{1, f};
-k=1;
-
-
-
-for i=1:5:100
+for f=1:length(data_set)
+   x_raw=data_set{1, f};
+   k=1;
    
-x_41= x_raw_41(:,i:i+19);
-a_41(k).k=x_41;
-k=k+1;
-end
+   for i=1:w_t:t-w_t   
+      x= x_raw(:,i:i+19);
+      a(k).k=x;
+      k=k+1;
+   end
 
+   clear correlations
+   clear Adj
 
-clear corr_41
-clear Adj_3s
-for i=1:20
-z_41= a_41(i);
-%previously, for was up to (527) 1000
-for j=1:64
-for l=1:64
-temp_corr_41=corrcoef(z_41.k(j,:),z_41.k(l,:));
-corr_41(j,l)=temp_corr_41(1,2);
-end
-end
-CORR_41(i).i=corr_41;
-disp('i')
-disp(i)
-end
+   for i=1:tau
+      z= a(i);
+      for j=1:num_voxel
+      for l=1:num_voxel
+         temp_corr=corrcoef(z.k(j,:),z.k(l,:));
+         correlations(j,l)=temp_corr(1,2);
+      end
+      end
+      CORRz(i).i=correlations;
+      disp('i')
+      disp(i)
+   end
 
-
-for i=1:20
-Adj_41{i,1}=CORR_41(i).i;
-end
-
-
-Adjacencies_Healthy_41_20_5{f}=Adj_41;
-
+   for i=1:tau
+      Adj{i,1}=CORRz(i).i;
+   end
+   
+   Dynamic_Adjacency{f}=Adj;
 end
