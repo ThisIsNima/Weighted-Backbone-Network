f2d6c0b8db893bf179fa3d55405d233ad6796e2d
ThisIsNima <42547511+ThisIsNima@users.noreply.github.com>
2021-04-27 20:51:57 -0400

    Create optimize_backbone.m

---
+++ optimize_backbone.m
@@ -0,0 +1,14 @@
+function F = optimize_backbone(std, std_vals) 
+
+%This is the optimizationfunction that is solved within the system of N equations 
+
+amat = std*std';
+
+MLmat=amat-std_vals;
+
+%MLmat = (tau.*amat)./(ones(Nd,Nd) - amat);
+
+
+MLmat(logical(eye(size(MLmat)))) = nan;% diagonal be nan.
+
+F = sum(MLmat,2,'omitnan');
